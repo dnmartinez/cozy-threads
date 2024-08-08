12e744fd18bcefce221148716b52558b714287cd
Naomi Martinez <dnmartinez94@gmail.com>
2024-08-08 03:35:57 -0500

    figuring out error

--- app/accessories/page.tsx
+++ app/accessories/page.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import MainLayout from '../mainLayout'
-import Link from 'next/link'
 import { getProducts } from '../lib/prisma'
 import { kumbhSans } from '../ui/fonts'
 
 export default async function Page() {
 	const products = await getProducts()
-	const accessories = products.filter(
-		(product) => product.category === 'ACCESSORIES'
-	)
+	const bottoms = products.filter((product) => product.category === 'BOTTOMS')
 
 	return (
 		<>
 			<MainLayout>
+				<div></div>
 				<div className='pt-28 bg-[#f8f9fb]'>
 					<h1
 						className={`text-3xl ${kumbhSans.className} font-light text-center mb-10`}
@@ -21,14 +19,14 @@ export default async function Page() {
 					</h1>
 
 					<div className='px-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
-						{accessories.map((accessory) => (
+						{bottoms.map((bottom) => (
 							<div
 								className='card rounded-none bg-[#f8f9fb] w-full shadow-md transition-transform duration-300 transform hover:scale-105 mb-4'
-								key={accessory.id}
+								key={bottom.id}
 							>
 								<figure>
 									<img
-										src={`${accessory.image}`}
+										src={`${bottom.image}`}
 										alt='Shoes'
 										className='w-full h-48 object-cover'
 									/>
@@ -37,13 +35,13 @@ export default async function Page() {
 									<h2
 										className={`card-title font-light border-b border-gray-300 ${kumbhSans.className} text-md`}
 									>
-										{`${accessory.title}`}
+										{`${bottom.title}`}
 									</h2>
 									<h3 className={`text-md mb-4 ${kumbhSans.className}`}>
-										$ {`${accessory.price}`}
+										$ {`${bottom.price}`}
 									</h3>
 									<div className='card-actions justify-end'>
-										<div className='badge badge-outline'>{`${accessory.category}`}</div>
+										<div className='badge badge-outline'>{`${bottom.category}`}</div>
 									</div>
 								</div>
 							</div>
